7a47806ab4fb9567de6ca9ba9299da3011885108
takuabonn <k.takumi19980322@gmail.com>
2022-06-26 02:38:12 +0900

    リファクタリング

--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,13 @@ export class Sugoroku {
       if (this.troutArray.length - 1 !== index) {
         newValue = newValue + a;
       }
-
-      if (newValue < 4) {
-        newArray.push(newValue);
-        return;
-      }
       if (newValue >= 4) {
         this.removedNumberCount = this.removedNumberCount + 1;
+        return;
       }
+      newArray.push(newValue);
     });
     this.troutArray = newArray;
-  }
-
-  private isLimit(num: number) {
-    if (num < 4) {
-      return false;
-    }
-    return true;
   }
 
   getTroutArray() {
